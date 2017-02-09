dad1e56f22b577578385b5a7474a26554d9a98cc
Jong Youl Choi <jyc@titan-ext4.ccs.ornl.gov>
2017-02-09 17:48:13 -0500

    Fixes for Titan

--- run-dataspaces.py
+++ run-dataspaces.py
@@ -37,8 +37,8 @@ def cmdlist(argv):
 
 def getstds(args):
     if args.oe is None:
-        f_stdout = open(args.stdout, 'w') if args.stdout is not None else subprocess.STDOUT
-        f_stderr = open(args.stderr, 'w') if args.stdout is not None else subprocess.STDERR
+        f_stdout = open(args.stdout, 'w') if args.stdout is not None else None
+        f_stderr = open(args.stderr, 'w') if args.stdout is not None else None
     else:
         f = open(args.oe, 'w')
         f_stdout = f
@@ -48,7 +48,7 @@ def getstds(args):
 def main():
     logging.basicConfig(level=logging.DEBUG, format='%(levelname)s: %(message)s')
     def usage():
-        print ('USAGE: run-dataspaces.py SERVER_COMMAND [ : APP_COMMAND ] *')
+        print ('USAGE: run-dataspaces.py <SERVER_COMMAND> [ : <APP_COMMAND> ]*')
         print ('====================')
         parser_svr.print_help()
         print ('--------------------')
@@ -61,11 +61,13 @@ def main():
     nclient = 0
     parser_svr = argparse.ArgumentParser(prog='SERVER_COMMAND', add_help=False)
     parser_svr.add_argument('--nserver', '-s', help='num. of servers', type=int, default=1)
+    parser_svr.add_argument('--nclient', '-c', help='num. of clients (will overwrite estimated number)', type=int, default=0)
     parser_svr.add_argument('--mpirun', help='mpirun command', default='mpirun')
     parser_svr.add_argument('--stdout', '-o', help='stdout')
     parser_svr.add_argument('--stderr', '-e', help='stderr')
     parser_svr.add_argument('--oe', help='merging stdout and stderr')
     parser_svr.add_argument('--dryrun', action='store_true', help='dryrun')
+    parser_svr.add_argument('--nompirun', action='store_true', help='no use of mpirun for clients') 
 
     parser_cmd = argparse.ArgumentParser(prog='APP_COMMAND', add_help=False)
     parser_cmd.add_argument('--np', '-n', help='num. of processes', type=int, default=1)
@@ -89,6 +91,9 @@ def main():
         setattr(args_cmd, 'UNKNOWN', _unknown)
         nclient += args_cmd.np
         args_cmd_list.append(args_cmd)
+
+    if args.nclient > 0:
+        nclient = args.nclient
 
     # Check config file and remove previous conf file
     if not os.path.exists('dataspaces.conf'):
@@ -118,15 +123,22 @@ def main():
         with open('conf') as f:
             lines = f.readlines()
             for ln in lines:
-                k, v = ln.rstrip('\n').split('=')
-                env[k] = v
+                try:
+                    k, v = ln.rstrip('\n').split('=')
+                    env[k] = v
+                except:
+                    pass
 
         logging.info('P2TNID=%s' % env['P2TNID'])
         logging.info('P2TPID=%s' % env['P2TPID'])
 
     # Run client
     for a in args_cmd_list:
-        cl_cmd = ' '.join(['mpirun -n %d' % (a.np), ' '.join(a.UNKNOWN), ' '.join(a.CMDS)])
+        if not args.nompirun:
+            cl_cmd = ' '.join(['%(mpirun)s -n %(np)d' % {'mpirun':args.mpirun, 'np':a.np}, ' '.join(a.UNKNOWN), ' '.join(a.CMDS)])
+        else:
+            cl_cmd = ' '.join([' '.join(a.CMDS)])
+
         logging.debug('CMD: %s' % cl_cmd)
         if not args.dryrun:
             f_stdout, f_stderr = getstds(a)
